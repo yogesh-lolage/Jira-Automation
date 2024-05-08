025ac104251c9e7fc0edca40c46881973bf3a3c5
Rajnesh Vishwakarma <rajnesh.vishwakarma@aress.com>
2024-05-08 15:25:50 +0530

    Add project files.

---
+++ Jira-Automation/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Jira_Automation.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
